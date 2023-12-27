5cd5e074f68a7b6d0ed698fa5c844837fb5acecc
bobablue <118743500+bobablue@users.noreply.github.com>
2023-12-27 14:40:47 +0800

    Changed script to behave as module

--- company/Deutsche Bank.py
+++ company/Deutsche Bank.py
@@ -3,17 +3,23 @@ import common.errorhandling as errorhandling
 import os
 import datetime
 
-#%% static data (71:UK, 189:SG, 1628:Frankfurt am Main, 1698:Frankfurt)
+#%% static data
+# 71:'United Kingdom', 189:'Singapore', 1628:'Frankfurt am Main', 1698:'Frankfurt'
 meta = {'urls':sf.get_urls('urls.csv', os.path.splitext(os.path.basename(__file__))[0]),
         'locations':{'PositionLocation.Country':[71, 189], 'PositionLocation.City':[1628, 1698]}}
+
+# requests parameters
+json_data = {'SearchParameters':{'CountItem':100,
+             'MatchedObjectDescriptor':['PositionID','PositionTitle','OrganizationName']},
+             'SearchCriteria':None}
 
 #%% functions
 #%%
 @errorhandling.data_error
 @sf.metadata(meta['urls']['company'], datetime.datetime.today().replace(microsecond=0))
-def jobs(json_obj):
+def extract_data(json_obj):
     key = 'MatchedObjectDescriptor'
-    data = [i[key] for i in response['SearchResult']['SearchResultItems'] if key in i]
+    data = [i[key] for i in json_obj['SearchResult']['SearchResultItems'] if key in i]
     data_dict = {}
 
     for d in data:
@@ -22,23 +28,22 @@ def jobs(json_obj):
 
     return(data_dict)
 
-#%% requests parameters
-json_data = {'SearchParameters':{'CountItem':100,
-             'MatchedObjectDescriptor':['PositionID','PositionTitle','OrganizationName']},
-             'SearchCriteria':None}
-
-#%% get all jobs
-jobs_dict = {}
-for i,j in meta['locations'].items():
-    json_data['SearchCriteria'] = [{'CriterionName':i, 'CriterionValue':j}]
-    response = sf.pull('get', json_decode=True, url=meta['urls']['page'], json=json_data)
-
-    # if no_jobs>default in CountItem, update json_data and call again with updated number
-    if response['SearchResult']['SearchResultCountAll']>json_data['SearchParameters']['CountItem']:
-        json_data['SearchParameters']['CountItem'] = response['SearchResult']['SearchResultCountAll']
+#%%
+def get_jobs():
+    jobs_dict = {}
+    for i,j in meta['locations'].items():
+        json_data['SearchCriteria'] = [{'CriterionName':i, 'CriterionValue':j}]
         response = sf.pull('get', json_decode=True, url=meta['urls']['page'], json=json_data)
 
-    jobs_dict.update(jobs(response))
-    del i,j
+        # if no_jobs>default in CountItem, update json_data and call again with updated number
+        if response['SearchResult']['SearchResultCountAll']>json_data['SearchParameters']['CountItem']:
+            json_data['SearchParameters']['CountItem'] = response['SearchResult']['SearchResultCountAll']
+            response = sf.pull('get', json_decode=True, url=meta['urls']['page'], json=json_data)
 
-sf.to_json(meta['urls']['company'], jobs_dict)
+        jobs_dict.update(extract_data(response))
+    return(jobs_dict)
+
+#%%
+if __name__=='__main__':
+    jobs_dict = get_jobs()
+    sf.to_json(meta['urls']['company'], jobs_dict)
