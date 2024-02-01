beec0155c644dbb649e1c9d4ba6e0d84005d8887
bobablue <118743500+bobablue@users.noreply.github.com>
2024-02-01 15:41:34 +0800

    Added more European banks

---
+++ company/ING.py
@@ -0,0 +1,56 @@
+import os
+import datetime
+from bs4 import BeautifulSoup
+
+# import custom scripts (https://stackoverflow.com/a/38455936)
+if __name__=='__main__' and __package__ is None:
+    os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from util import scrape_funcs, error_handling
+
+#%% static data
+meta = {'urls':scrape_funcs.get_urls(os.path.join(os.path.dirname(__file__), 'urls.csv'), os.path.splitext(os.path.basename(__file__))[0]),
+
+        'requests':{'url':{'location_level_1':'SG', 'start':0}}}
+
+#%% functions
+#%%
+@error_handling.data_error
+@scrape_funcs.metadata(meta['urls']['company'], datetime.datetime.today().replace(microsecond=0))
+def jobs(bs_obj):
+    data = bs_obj.find_all('div', class_='careers-search-result')
+    data_dict = {}
+    for d in data:
+        data_dict[meta['urls']['job']+d.find('a')['href']] = {'Title':d.find('a').text,
+                                                              'Location':d.find(class_='meta-information').text.split(' | ')[-3],
+                                                              'Job Function':d.find(class_='meta-information').text.split(' | ')[1]}
+    return(data_dict)
+
+#%%
+@scrape_funcs.track_status(__file__)
+def get_jobs():
+    # get total number of jobs and pages to loop through and parse first page of results
+    response = scrape_funcs.pull('get', url=meta['urls']['page'], params=meta['requests']['url'])
+    bs_obj = BeautifulSoup(response.content, 'html.parser')
+
+    # parse first page
+    jobs_dict = {}
+    jobs_dict.update(jobs(bs_obj))
+
+    # pagesize calculated using number of jobs posted on first page, num_jobs can be <50 even if default is 50
+    num_jobs = int(bs_obj.find_all('strong')[1].text)
+    pagesize = len(jobs_dict)
+    pages = num_jobs//pagesize + (num_jobs % pagesize>0)
+
+    # compile jobs from all pages after first, into main dict (update start number in meta['requests']['url'])
+    for pg in range(1, pages):
+        meta['requests']['url']['start'] = pg*50
+        response = scrape_funcs.pull('get', url=meta['urls']['page'], params=meta['requests']['url'])
+        bs_obj = BeautifulSoup(response.content, 'html.parser')
+        jobs_dict.update(jobs(bs_obj))
+
+    return(jobs_dict)
+
+#%%
+if __name__=='__main__':
+    jobs_dict = get_jobs()
+    scrape_funcs.to_json(meta['urls']['company'], jobs_dict)
---
+++ company/Societe Generale.py
@@ -0,0 +1,66 @@
+import os
+import datetime
+
+# import custom scripts (https://stackoverflow.com/a/38455936)
+if __name__=='__main__' and __package__ is None:
+    os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from util import scrape_funcs, error_handling
+
+#%% static data
+meta = {'urls':scrape_funcs.get_urls(os.path.join(os.path.dirname(__file__), 'urls.csv'), os.path.splitext(os.path.basename(__file__))[0]),
+
+        'requests':{'cookie':{'headers':{'X-Proxy-URL':'https://sso.sgmarkets.com/sgconnect/oauth2/access_token',
+                                         'Authorization-API':''},
+
+                              'data':{'grant_type':'client_credentials', 'scope':'api.corpsrc-00257.v1'}},
+
+                    'headers':{'X-Proxy-URL':'https://api.socgen.com/business-support/it-for-it-support/cognitive-service-knowledge/api/v1/search-profile'},
+
+                    'json':{'profile':'ces_profile_sgcareers',
+                            'responseType':'SearchResult',
+                            'query':{'skipCount':100,'skipFrom':0,
+                                     'advanced':[{'type':'simple','name':'sourcestr6','op':'eq','value':'job'},
+                                                 {'type':'multi','name':'sourcecsv1','op':'eq','values':['SGP']}]}}}}
+
+#%% functions
+#%%
+@error_handling.data_error
+@scrape_funcs.metadata(meta['urls']['company'], datetime.datetime.today().replace(microsecond=0))
+def jobs(json_obj):
+    data_dict = {}
+    data_dict[json_obj['resulturl']] = {'Title':json_obj['title'],
+                                        'Job Function':json_obj['sourcestr10'],
+                                        'Location':json_obj['sourcecsv1']}
+    return(data_dict)
+
+#%%
+@scrape_funcs.track_status(__file__)
+def get_jobs():
+    # get cookie and access token
+    cookie = scrape_funcs.pull('post', url=meta['urls']['page'],
+                               headers=meta['requests']['cookie']['headers'], data=meta['requests']['cookie']['data'])
+
+    cookie = {**cookie.json(), **cookie.cookies.get_dict()}
+
+    # update headers with cookie token
+    headers = meta['requests']['headers'] | {'Authorization-API':f"{cookie['token_type']} {cookie['access_token']}"}
+
+    # get all jobs
+    response = scrape_funcs.pull('post', url=meta['urls']['page'], headers=headers, json=meta['requests']['json']).json()
+
+    # if num_jobs>default in skipCount, update json_data and call again with updated number
+    num_jobs = response['TotalCount']
+    if num_jobs>meta['requests']['json']['query']['skipCount']:
+        meta['requests']['json']['query']['skipCount'] = num_jobs
+        response = scrape_funcs.pull('post', url=meta['urls']['page'], headers=headers, json=meta['requests']['json']).json()
+
+    jobs_dict = {}
+    for i in response['Result']['Docs']:
+        jobs_dict.update(jobs(i))
+
+    return(jobs_dict)
+
+#%%
+if __name__=='__main__':
+    jobs_dict = get_jobs()
+    scrape_funcs.to_json(meta['urls']['company'], jobs_dict)
