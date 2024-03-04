1ae928b23fe54caf06ca2d400635446ae18a1479
bobablue <118743500+bobablue@users.noreply.github.com>
2024-03-04 10:17:34 +0800

    Updated script in response to Wise's API change

--- company/Wise.py
+++ company/Wise.py
@@ -1,5 +1,7 @@
 import os
 import datetime
+from bs4 import BeautifulSoup
+import re
 
 # import custom scripts (https://stackoverflow.com/a/38455936)
 if __name__=='__main__' and __package__ is None:
@@ -8,32 +10,58 @@ from util import scrape_funcs, error_handling
 
 #%% static data
 meta = {'urls':scrape_funcs.get_urls(os.path.join(os.path.dirname(__file__), 'urls.csv'), os.path.splitext(os.path.basename(__file__))[0]),
+        'job_max':500,
 
-        'requests':{'url':{'offices':[14], 'page':1}}} # need to loop by location if offices>1
+        'requests':{'headers':{'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:123.0) Gecko/20100101 Firefox/123.0'},
+
+                    'url':{'options':[320], 'page':1}}}
+
+meta['requests']['url']['size'] = meta['job_max']
 
 #%% functions
 #%%
 @error_handling.data_error
 @scrape_funcs.metadata(meta['urls']['company'], datetime.datetime.today().replace(microsecond=0))
-def jobs(json_dict):
+def jobs(bs_obj):
+    titles = bs_obj.find_all('a', class_='attrax-vacancy-tile__title attrax-vacancy-tile__item attrax-button')
+    links = [i['href'] for i in titles]
+    titles = [i.text.strip() for i in titles]
+
+    job_funcs = bs_obj.find_all('div', class_='attrax-vacancy-tile__option-team-valueset attrax-vacancy-tile__item-valueset')
+    job_funcs = [i.find('p', class_='attrax-vacancy-tile__item-value').text.strip() for i in job_funcs]
+
+    locs = bs_obj.find_all('div', class_='attrax-vacancy-tile__location-freetext attrax-vacancy-tile__item')
+    locs = [i.find('p', class_='attrax-vacancy-tile__item-value').text.strip() for i in locs]
+
+    data = zip(links, titles, job_funcs, locs)
     data_dict = {}
-    for i in json_dict:
-        data_dict[i['permalink']] = {'Title':scrape_funcs.decode(i['title']),
-                                     'Job Function':i['team'],
-                                     'Location':i['office']}
+    for i in data:
+        data_dict[meta['urls']['job'] + i[0]] = {'Title':i[1], 'Job Function':i[2], 'Location':i[3]}
+
     return(data_dict)
 
 #%%
 @scrape_funcs.track_status(__file__)
 def get_jobs():
-    response = scrape_funcs.pull('get', url=meta['urls']['page'], params=meta['requests']['url'], json_decode=True)
-    pages = response['data']['max_pages']
-    jobs_dict = jobs(response['data']['posts']) # parse first page
+    response = scrape_funcs.pull('get', url=meta['urls']['page'], headers=meta['requests']['headers'],
+                                 params=meta['requests']['url'])
+
+    bs_obj = BeautifulSoup(response.content, 'html.parser')
+
+    num_jobs = bs_obj.find('span', class_='attrax-pagination__total-results').text.strip()
+    num_jobs = int(re.search(r'(\d+) results', num_jobs).group(1))
+    pagesize = meta['job_max']
+    pages = num_jobs//pagesize + (num_jobs % pagesize>0)
+
+    jobs_dict = jobs(bs_obj) # parse first page
 
     for i in range(2,pages+1):
         meta['requests']['url']['page'] = i
-        response = scrape_funcs.pull('get', url=meta['urls']['page'], params=meta['requests']['url'], json_decode=True)
-        jobs_dict.update(jobs(response['data']['posts']))
+        response = scrape_funcs.pull('get', url=meta['urls']['page'], headers=meta['requests']['headers'],
+                                     params=meta['requests']['url'])
+
+        bs_obj = BeautifulSoup(response.content, 'html.parser')
+        jobs_dict.update(jobs(bs_obj))
 
     return(jobs_dict)
 
