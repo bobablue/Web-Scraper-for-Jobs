09dcfd05c2c67ee37ed13a22bd95ca8694eb9f16
bobablue <118743500+bobablue@users.noreply.github.com>
2024-03-07 18:12:43 +0800

    Added Blackstone and PwC

---
+++ company/PwC.py
@@ -0,0 +1,43 @@
+import os
+import datetime
+import re
+import json
+
+# import custom scripts (https://stackoverflow.com/a/38455936)
+if __name__=='__main__' and __package__ is None:
+    os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from util import scrape_funcs, error_handling
+
+#%% static data
+meta = {'urls':scrape_funcs.get_urls(os.path.join(os.path.dirname(__file__), 'urls.csv'), os.path.splitext(os.path.basename(__file__))[0]),
+
+        'requests':{'url':{'wdcountry':'SGP'}}}
+
+#%% functions
+#%%
+@error_handling.data_error
+@scrape_funcs.metadata(meta['urls']['company'], datetime.datetime.today().replace(microsecond=0))
+def jobs(json_obj):
+    data_dict = {}
+    for i in json_obj:
+        data_dict[meta['urls']['job'] + i['jobreqid']] = {'Title':i['title'],
+                                                          'Job Function':i['specialism'],
+                                                          'Location':i['location']}
+    return(data_dict)
+
+#%%
+@scrape_funcs.track_status(__file__)
+def get_jobs():
+    response = scrape_funcs.pull('get', url=meta['urls']['page'], params=meta['requests']['url'])
+
+    # json object embedded within html
+    json_obj = re.search(r'var jsondata = (.+) ;', response.text).group(1)
+    json_obj = json.loads(json_obj)
+
+    jobs_dict = jobs(json_obj)
+    return(jobs_dict)
+
+#%%
+if __name__=='__main__':
+    jobs_dict = get_jobs()
+    scrape_funcs.to_json(meta['urls']['company'], jobs_dict)
