cc0dba9fe7a90b76e5f301d8e45975c98efc94cc
bobablue <118743500+bobablue@users.noreply.github.com>
2024-03-23 17:11:32 +0800

    Added Databricks

---
+++ company/Databricks.py
@@ -0,0 +1,50 @@
+import os
+import datetime
+
+# import custom scripts (https://stackoverflow.com/a/38455936)
+if __name__=='__main__' and __package__ is None:
+    os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from util import scrape_funcs, error_handling
+
+#%% static data
+meta = {'urls':scrape_funcs.get_urls(os.path.join(os.path.dirname(__file__), 'urls.csv'), os.path.splitext(os.path.basename(__file__))[0]),
+        'locations':['singapore']}
+
+#%% functions
+#%%
+def collapse_list_dict(obj):
+    coll = [x for v in [list(i.values()) for i in obj] for x in v]
+    coll = ' | '.join(coll)
+    return(coll)
+
+#%%
+@error_handling.data_error
+@scrape_funcs.metadata(meta['urls']['company'], datetime.datetime.today().replace(microsecond=0))
+def jobs(json_obj):
+    data = json_obj['result']['pageContext']['data']['allGreenhouseDepartment']['nodes']
+    jobs_all = {}
+
+    # jobs found in ['jobs'] and ['parent']['jobs'] but they overlap so don't need to capture ['parent']['jobs']
+    for i in data:
+
+        if i['jobs']:
+            for j in i['jobs']:
+                jobs_all[j['absolute_url']] = {'Title':j['title'],
+                                               'Job Function':collapse_list_dict(j['departments']),
+                                               'Location':j['location']['name']}
+
+    # filter to only locations selected
+    jobs_filtered = {k:v for k,v in jobs_all.items() if any(x in v['Location'].lower() for x in meta['locations'])}
+    return(jobs_filtered)
+
+#%%
+@scrape_funcs.track_status(__file__)
+def get_jobs():
+    response = scrape_funcs.pull('get', url=meta['urls']['page'], json_decode=True)
+    jobs_dict = jobs(response)
+    return(jobs_dict)
+
+#%%
+if __name__=='__main__':
+    jobs_dict = get_jobs()
+    scrape_funcs.to_json(meta['urls']['company'], jobs_dict)
