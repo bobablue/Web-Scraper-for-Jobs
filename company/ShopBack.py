6fd2f6d66e0429b8b4d76357c244bc3b41c58cb6
bobablue <118743500+bobablue@users.noreply.github.com>
2024-06-30 22:55:59 +0800

    Added Shopify, ShopBack

---
+++ company/ShopBack.py
@@ -0,0 +1,46 @@
+import os
+import datetime
+
+# import custom scripts (https://stackoverflow.com/a/38455936)
+if __name__=='__main__' and __package__ is None:
+    os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from util import scrape_funcs, error_handling
+
+#%% static data
+meta = {'urls':scrape_funcs.get_urls(os.path.join(os.path.dirname(__file__), 'urls.csv'), os.path.splitext(os.path.basename(__file__))[0]),
+        'locations':['singapore'],
+
+        'requests':{'url':{'group':'team', 'mode':'json'}}}
+
+#%% functions
+#%%
+@error_handling.data_error
+@scrape_funcs.metadata(meta['urls']['company'], datetime.datetime.today().replace(microsecond=0))
+def jobs(json_obj):
+    data_dict = {}
+    for i in json_obj:
+        for job in i['postings']:
+
+            locs = job['categories']['allLocations']
+            locs = sorted(locs, key=lambda x:(x.split(',')[-1].strip(), x.split(',')[0]))
+            locs = ' | '.join(locs)
+
+            data_dict[job['hostedUrl']] = {'Title':job['text'], 'Job Function':i['title'], 'Location':locs}
+
+    data_dict = scrape_funcs.clean_loc(data_dict)
+
+    # restrict to singapore only
+    data_dict = {k:v for k,v in data_dict.items() if any(x in v['Location'].lower() for x in meta['locations'])}
+    return(data_dict)
+
+#%%
+@scrape_funcs.num_jobs(__file__)
+def get_jobs():
+    response = scrape_funcs.pull('get', url=meta['urls']['page'], params=meta['requests']['url'], json_decode=True)
+    jobs_dict = jobs(response)
+    return(jobs_dict)
+
+#%%
+if __name__=='__main__':
+    jobs_dict = get_jobs()
+    scrape_funcs.to_json(meta['urls']['company'], jobs_dict)
---
+++ company/Shopify.py
@@ -0,0 +1,40 @@
+import os
+import datetime
+
+# import custom scripts (https://stackoverflow.com/a/38455936)
+if __name__=='__main__' and __package__ is None:
+    os.sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from util import scrape_funcs, error_handling
+
+#%% static data
+meta = {'urls':scrape_funcs.get_urls(os.path.join(os.path.dirname(__file__), 'urls.csv'), os.path.splitext(os.path.basename(__file__))[0]),
+        'locations':['global','apac'], # fully remote opportunities, so it's listed by region and not country
+
+        'requests':{'url':{'_data':'pages/shopify.com/($locale)/careers'}}}
+
+#%% functions
+#%%
+@error_handling.data_error
+@scrape_funcs.metadata(meta['urls']['company'], datetime.datetime.today().replace(microsecond=0))
+def jobs(json_obj):
+    data_dict = {}
+    for i in json_obj:
+        data_dict[i['jobPosting']['externalLink']] = {'Title':i['jobPosting']['title'],
+                                                      'Job Function':i['job']['title'],
+                                                      'Location':i['jobPosting']['locationName']}
+
+    # restrict to selected locations
+    data_dict = {k:v for k,v in data_dict.items() if v['Location'].lower() in meta['locations']}
+    return(data_dict)
+
+#%%
+@scrape_funcs.num_jobs(__file__)
+def get_jobs():
+    response = scrape_funcs.pull('get', url=meta['urls']['page'], params=meta['requests']['url'], json_decode=True)
+    jobs_dict = jobs(response['jobPostingsWithJobs'])
+    return(jobs_dict)
+
+#%%
+if __name__=='__main__':
+    jobs_dict = get_jobs()
+    scrape_funcs.to_json(meta['urls']['company'], jobs_dict)
