0a53677386c732637f5f5f023682bc00c50c7ca1
bobablue <118743500+bobablue@users.noreply.github.com>
2024-07-29 22:04:18 +0800

    Old API retired, set new as main API

--- company/Standard Chartered.py
+++ company/Standard Chartered.py
@@ -1,6 +1,6 @@
 import os
 import datetime
-import re
+import xml.etree.ElementTree as et
 
 # import custom scripts (https://stackoverflow.com/a/38455936)
 if __name__=='__main__' and __package__ is None:
@@ -9,49 +9,30 @@ from util import scrape_funcs, error_handling
 
 #%% static data
 meta = {'urls':scrape_funcs.get_urls(os.path.join(os.path.dirname(__file__), 'urls.csv'), os.path.splitext(os.path.basename(__file__))[0]),
-
-        'requests':{'headers':{'Content-Type':'application/x-www-form-urlencoded'},
-
-                    # 209 fields in full POST data but only these are necessary to get all data
-                    'post':{'location1':'6405120195', # singapore
-                            'dropListSize':100,
-                            'ftlinterfaceid':'requisitionListInterface',
-                            'ftlcompid':'validateTimeZoneId',
-                            'ftlcompclass':'InitTimeZoneAction'}}}
+        'locations':['singapore']}
 
 #%% functions
 #%%
 @error_handling.data_error
 @scrape_funcs.metadata(meta['urls']['company'], datetime.datetime.today().replace(microsecond=0))
 def jobs(response_obj):
-    jobs_text = response_obj.text.split('!|!')
+    xml_obj = et.fromstring(response_obj.text)
+    jobs = xml_obj.find('jobs')
 
-    # title, url, job function, location
-    data_indices = [(i,i+6,i+7,i+8) for i in range(8, len(jobs_text)+1, 45)]
-
-    # clean up strings before adding to dict
     data_dict = {}
-    for idx in data_indices:
-        if jobs_text[idx[1]].isnumeric():
-            data_dict[meta['urls']['job']+jobs_text[idx[1]]] = {'Title':scrape_funcs.decode(jobs_text[idx[0]]),
-                                                                'Job Function':scrape_funcs.decode(jobs_text[idx[2]]),
-                                                                'Location':scrape_funcs.decode(jobs_text[idx[3]])}
+    for job in jobs:
+        data_dict[job.find('url').text] = {'Title':job.find('title').text,
+                                           'Location':job.find('location').text,
+                                           'Job Function':job.find('adcode').text}
 
+    data_dict = scrape_funcs.restrict_loc(data_dict, meta['locations'])
+    data_dict = scrape_funcs.clean_loc(data_dict)
     return(data_dict)
 
 #%%
 @scrape_funcs.num_jobs(__file__)
 def get_jobs():
-    response = scrape_funcs.pull('post', url=meta['urls']['page'],
-                                 headers=meta['requests']['headers'], data=meta['requests']['post'])
-
-    # if num_jobs>default in dropListSize, update post_data and call again with updated number
-    num_jobs = int(re.compile(r'listRequisition.nbElements!\|!(\d+)').findall(response.text)[0])
-    if num_jobs>meta['requests']['post']['dropListSize']:
-        meta['requests']['post']['dropListSize'] = num_jobs
-        response = scrape_funcs.pull('post', url=meta['urls']['page'],
-                                     headers=meta['requests']['headers'], data=meta['requests']['post'])
-
+    response = scrape_funcs.pull('get', url=meta['urls']['page'])
     jobs_dict = jobs(response)
     return(jobs_dict)
 
